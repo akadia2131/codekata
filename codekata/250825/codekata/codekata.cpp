ee4df25977606f20f47b1e642c7ca185a7692044
akadia2131 <akadia2131@gmail.com>
2025-08-25 09:39:29 +0900

    Add files via upload

---
+++ codekata/250825/codekata/codekata.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int angle) {
+    int answer = 0;
+    answer = angle < 90 ? 1 : (angle == 90 ? 2 : (angle < 180 ? 3 : 4));
+    return answer;
+}
