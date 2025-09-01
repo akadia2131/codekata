6ff5cf679c670db7a5e6c1673bad43d38a617675
akadia2131 <akadia2131@gmail.com>
2025-09-01 10:07:43 +0900

    Add files via upload

---
+++ codekata/250830/codekata/codekata.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+
+    while (n != 0)
+    {
+        if (n % 10 == 0)
+        {
+            n /= 10;
+        }
+        else
+        {
+            n--;
+            answer++;
+        }
+    }
+
+    return answer;
+}
