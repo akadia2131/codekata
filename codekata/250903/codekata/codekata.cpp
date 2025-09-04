27854a0d05eca3569ca6e2814fde4321fb92a9f0
akadia2131 <akadia2131@gmail.com>
2025-09-04 10:14:40 +0900

    Add files via upload

---
+++ codekata/250903/codekata/codekata.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(long long n) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    int* answer = (int*)malloc(sizeof(int) * 11);
+
+    int sum = 0;
+
+    for (int i = 0; n > 0; i++)
+    {
+        sum = n % 10;
+        n /= 10;
+        answer[i] = sum;
+    }
+
+    return answer;
+}
