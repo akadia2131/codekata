a0d1601f4d8b6d3d1bb1af9b4bf179d18d3741cf
akadia2131 <akadia2131@gmail.com>
2025-09-04 10:29:36 +0900

    Add files via upload

---
+++ codekata/250904/codekata/codekata.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* s) {
+    int answer = 0;
+
+    const char* sum = "%77";
+
+    const char* result = strstr(s, sum);
+
+    answer = atoi(s);
+
+    if (result != NULL)
+    {
+        answer = answer - answer - answer;
+    }
+
+    return answer;
+}
