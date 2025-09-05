ed9f97ee7af9063634fc22835839e088d69b159e
akadia2131 <akadia2131@gmail.com>
2025-09-05 11:33:37 +0900

    Add files via upload

---
+++ codekata/250905/codekata/codekata.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(long long n) {
+    long long answer = -1;
+
+    for (long long i = 0; i < n; i++)
+    {
+        if (i * i == n)
+        {
+            i++;
+            answer = i * i;
+            break;
+        }
+    }
+
+    if (n == 1) answer = 4;
+
+
+    return answer;
+}
