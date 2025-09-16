1f46566377150d611a55985758b2eb068066ceae
akadia2131 <akadia2131@gmail.com>
2025-09-16 09:52:45 +0900

    Add files via upload

---
+++ codekata/250916/codekata/codekata.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+
+    int len = s.length();
+
+    if (len % 2 == 1) // 문자열 길이가 홀수
+    {
+        answer = s.substr(len / 2, 1);
+    }
+    else // 문자열 길이가 짝수
+    {
+        answer = s.substr((len / 2) - 1, 2);
+    }
+
+    return answer;
+}
