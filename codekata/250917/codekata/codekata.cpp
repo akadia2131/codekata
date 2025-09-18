10dd2d0e9937159ef34649d6f0cf35113761a669
akadia2131 <akadia2131@gmail.com>
2025-09-18 10:11:58 +0900

    Add files via upload

---
+++ codekata/250917/codekata/codekata.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+
+    string sum1 = "수";
+    string sum2 = "박";
+    bool i = true;
+
+    while (n > 0)
+    {
+        if (i == true)
+        {
+            answer += sum1;
+            i = false;
+        }
+        else if (i == false)
+        {
+            answer += sum2;
+            i = true;
+        }
+        n--;
+    }
+
+    return answer;
+}
